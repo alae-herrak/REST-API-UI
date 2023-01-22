f94d23a2343e3898353b42a455235774d41f5817
alaeh-errak <alaeherrak@gmail.com>
2023-01-22 21:32:39 +0100

    finished get logic

--- src/components/Get.jsx
+++ src/components/Get.jsx
@@ -1,10 +1,37 @@
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import JsonFormatter from "react-json-formatter";
 
 const Get = () => {
   const isConnected = useSelector((state) => state.isConnected.isConnected);
+  const port = useSelector((state) => state.port.port);
+  const infoType = useSelector((state) => state.infoType.infoType);
   const [searchType, setSearchType] = useState("all");
+  const [searchId, setSearchId] = useState();
+  const [data, setData] = useState([]);
+
+  const jsonStyle = {
+    propertyStyle: { color: "red" },
+    stringStyle: { color: "green" },
+    numberStyle: { color: "darkorange" },
+  };
+
+  const handleGet = async (searchType, searchId) => {
+    switch (searchType) {
+      case "all":
+        const allData = await axios.get(`http://localhost:${port}/${infoType}`);
+        setData(allData.data);
+        break;
+      case "byId":
+        const dataById = await axios.get(
+          `http://localhost:${port}/${infoType}/${searchId}`
+        );
+        setData(dataById.data);
+        break;
+    }
+  };
 
   return (
     <div className="get">
@@ -25,8 +52,25 @@ const Get = () => {
           <option value="all">all</option>
           <option value="byId">by id</option>
         </select>
-        <input type="text" disabled={searchType === "all" ? "disabled" : ""} />
-        <button>Send</button>
+        <input
+          type="text"
+          disabled={searchType === "all" ? "disabled" : ""}
+          onChange={(e) => {
+            setSearchId(e.target.value);
+          }}
+        />
+        <button onClick={() => handleGet(searchType, searchId)}>Send</button>
+      </div>
+      <div className="get-show">
+        {data.length === 0 ? (
+          ""
+        ) : (
+          <JsonFormatter
+            json={JSON.stringify(data)}
+            tabWith={4}
+            jsonStyle={jsonStyle}
+          />
+        )}
       </div>
     </div>
   );
